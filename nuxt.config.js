624c5383d79545a0c6a9d0a7266719e7f96716b1
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2023-08-31 23:45:05 +0200

    updated pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: "WD Blog",
+    title: "Post something",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
